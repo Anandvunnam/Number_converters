c977e7f287541d59f9ac6afe3faf537119c19440
Anandvunnam <anand.vunnam2000@gmail.com>
2021-06-21 20:20:04 +0530

    Initialising DOM variables.

---
+++ script.js
@@ -0,0 +1,9 @@
+var from = document.getElementById("from")
+var to = document.getElementById("to")
+var btn = document.querySelector(".btn")
+btn.addEventListener("click", convert())
+var input = document.getElementById("input")
+
+function convert() {
+
+}

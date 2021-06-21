ef243aa6a66ee67d9a81d14ec76d975b86d7cb74
Anandvunnam <anand.vunnam2000@gmail.com>
2021-06-21 23:15:45 +0530

    Added some onchange event listeners and introducted makeAlert funtion and some error handlers.

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 var from = document.getElementById("from")
 var to = document.getElementById("to")
-var btn = document.querySelector(".btn")
-btn.addEventListener("click", convert())
+var btn = document.querySelector(".btn.btn-primary")
+var showAlert = document.querySelector(".alert")
+var showAlertMessage = document.querySelector(".alert .error-msg")
 var input = document.getElementById("input")
 
-function convert() {
+// Event listners.
+from.addEventListener("change", function () { makeAlert("none", "") })
+to.addEventListener("change", function () { makeAlert("none", "") })
+input.addEventListener("change", function () { makeAlert("none", "") })
+btn.addEventListener("click", function () { checkErrors() })
 
+// To display alerta and error messages.
+makeAlert = (display, message) => {
+    showAlert.style.display = display
+    showAlertMessage.innerText = message
+}
+
+
+// Check various errors and display alerts
+function checkErrors() {
+    if (from.value === to.value) { // if both values are same 
+        makeAlert("block", "From and To values cannot be same.")
+    }
+
+    else if (from.value === "From" || to.value === "To") { // if both values are set to initial ones.
+        makeAlert("block", "Please select the conversions.")
+    }
+    else if (input.value === "") { // if input is empty
+        makeAlert("block", "Enter any input value to convert")
+    }
 }

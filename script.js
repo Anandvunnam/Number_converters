884bf168276190dfd01ab2fed6224d79410492f5
Bhavani <bhavaninaidu787831@gmail.com>
2021-06-22 17:12:30 +0530

    Added Binary To Decimal Conversion and DOM for output

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var btn = document.querySelector(".btn.btn-primary")
 var showAlert = document.querySelector(".alert")
 var showAlertMessage = document.querySelector(".alert .error-msg")
 var input = document.getElementById("input")
-
+var output =document.querySelector(".wrapper [readonly]")
 // Event listners.
 from.addEventListener("change", function () { makeAlert("none", "") })
 to.addEventListener("change", function () { makeAlert("none", "") })
@@ -30,4 +30,24 @@ function checkErrors() {
     else if (input.value === "") { // if input is empty
         makeAlert("block", "Enter any input value to convert")
     }
+    else if(from.value === "binary" && to.value === "decimal")
+    {
+        var deci=binaryToDecimal()
+        output.value=deci
+    }
+}
+function binaryToDecimal()
+{
+     var binary=Number(input.value)
+     var rem=0
+     var num=1
+     var decimal=0;
+     while(binary>0)
+     {
+        rem=binary%10
+        binary=Math.floor(binary/10)
+        decimal+=rem*num
+        num*=2
+     }
+     return decimal
 }

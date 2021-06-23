aaaf10051962ae388dc4a74117b059324e83e0f1
Anandvunnam <anand.vunnam2000@gmail.com>
2021-06-23 13:35:52 +0530

    added convertNumbers,heaxdecimal to octal and octal to decimal methods with regex checking.

--- script.js
+++ script.js
@@ -4,17 +4,52 @@ var btn = document.querySelector(".btn.btn-primary")
 var showAlert = document.querySelector(".alert")
 var showAlertMessage = document.querySelector(".alert .error-msg")
 var input = document.getElementById("input")
-var output =document.querySelector(".wrapper [readonly]")
+var output = document.querySelector(".wrapper [readonly]")
 // Event listners.
 from.addEventListener("change", function () { makeAlert("none", "") })
 to.addEventListener("change", function () { makeAlert("none", "") })
-input.addEventListener("change", function () { makeAlert("none", "") })
+input.addEventListener("change", function () {
+    makeAlert("none", "")
+    output.value = ""
+})
 btn.addEventListener("click", function () { checkErrors() })
 
 // To display alerta and error messages.
 makeAlert = (display, message) => {
     showAlert.style.display = display
     showAlertMessage.innerText = message
+}
+
+
+convertNumbers = (from, to, input) => {
+    var fromToString = from + to
+    switch (fromToString) {
+        case 'decimalbinary':
+            output.value = decimalToBinary(input); break;
+        case 'decimaloctal':
+            output.value = decimalToOctal(input); break;
+        case 'decimalhexadecimal':
+            output.value = decimalToHexadecimal(input); break;
+        case 'binarydecimal':
+            output.value = binaryToDecimal(input); break;
+        case 'binaryoctal':
+            output.value = binaryToOctal(input); break;
+        case 'binaryhexadecimal':
+            output.value = binaryToHexadecimal(input); break;
+        case 'octaldecimal':
+            output.value = octalToDecimal(input); break;
+        case 'octalbinary':
+            output.value = octalToBinary(input); break;
+        case 'octalhexadecimal':
+            output.value = octalToHexadecimal(input); break;
+        case 'hexadecimaldecimal':
+            output.value = hexadecimalToDecimal(input); break;
+        case 'hexadecimalbinary':
+            output.value = hexadecimalToBinary(input); break;
+        case 'hexadecimaloctal':
+            output.value = hexadecimalToOctal(input); break;
+        default: makeAlert("block", "Invalid Conversion!")
+    }
 }
 
 
@@ -30,70 +65,90 @@ function checkErrors() {
     else if (input.value === "") { // if input is empty
         makeAlert("block", "Enter any input value to convert")
     }
-    else if(from.value === "binary" && to.value === "decimal")
-    {
-        var deci=binaryToDecimal()
-        output.value=deci
+    else {
+        convertNumbers(from.value, to.value, input.value)
     }
-    else if (from.value === "hexadecimal" && to.value ==="decimal")
-    {
-        var deci=hexadecimalToDecimal()
-        output.value=deci
-    }
-    else if (from.value === "hexadecimal" && to.value === "octal")
-    {
-        var deci=hexadecimalToDecimal()
-        
-        var octal=decimalToOctal(deci)
-        output.value=octal
-    }
+}
 
-}
-function binaryToDecimal()
-{
-    var binary=Number(input.value)
-    var rem=0
-    var num=1
-    var decimal=0;
-    while(binary>0)
-    {
-        rem=binary%10
-        binary=Math.floor(binary/10)
-        decimal+=rem*num
-        num*=2
+
+function binaryToDecimal(binaryNumber) {
+    var rem = 0
+    var num = 1
+    var decimal = 0;
+    while (binaryNumber > 0) {
+        rem = binaryNumber % 10
+        binaryNumber = Math.floor(binaryNumber / 10)
+        decimal += rem * num
+        num *= 2
     }
     return decimal
 }
-function hexadecimalToDecimal()
-{
-    var hexanum=input.value
-    var num=1
-    var decimal=0
-    for(var i=hexanum.length-1;i>=0;i--)
-    {
-        if(hexanum[i]>='0' && hexanum[i]<='9')
-        {
-            decimal+=(hexanum.charCodeAt(i)-48)*num
+
+function hexadecimalToDecimal(number) {
+    var hexanum = number
+    var num = 1
+    var decimal = 0
+    for (var i = hexanum.length - 1; i >= 0; i--) {
+        if (hexanum[i] >= '0' && hexanum[i] <= '9') {
+            decimal += (hexanum.charCodeAt(i) - 48) * num
         }
-        else
-        {
-            decimal+=(hexanum.charCodeAt(i)-55)*num;
+        else {
+            decimal += (hexanum.charCodeAt(i) - 55) * num;
         }
-        num*=16
+        num *= 16
     }
     return decimal
 }
-function decimalToOctal(decimal)
-{
-    var octal=0
-    var rem=0
-    var num=1
-    while(decimal>0)
-    {
-        rem=decimal%8
-        decimal=Math.floor(decimal/8)
-        octal+=rem*num
-        num*=10
+
+function decimalToOctal(decimal) {
+    var octal = 0
+    var rem = 0
+    var num = 1
+    while (decimal > 0) {
+        rem = decimal % 8
+        decimal = Math.floor(decimal / 8)
+        octal += rem * num
+        num *= 10
     }
     return octal
 }
+
+function decimalToBinary(number) {
+    var pattern = /[0-9]*/g
+    if (number !== pattern.exec(number)[0]) return makeAlert("block", "Decimal range should be in [0-9].")
+    var result = 0
+    var place = 0
+    while (number > 0) {
+        result += Math.pow(10, place++) * (number % 2)
+        number = Math.floor(number / 2)
+    }
+    return result
+}
+
+function decimalToHexadecimal(number) {
+    var result = new Array()
+    let i = 0
+    while (number > 0) {
+        result[i++] = number % 16
+        number = Math.floor(number / 16)
+    }
+    hexValue = ""
+    for (let j = result.length - 1; j >= 0; j--) {
+        hexValue += (result[j] < 10) ? String.fromCharCode(result[j] + 48) : String.fromCharCode(result[j] + 55)
+    }
+    return hexValue
+}
+
+function octalToDecimal(number) {
+    var pattern = /[0-7]+/g
+    if (number !== pattern.exec(number)[0]) {
+        return makeAlert("block", "Octal digit range should be [0-7]")
+    }
+    let result = 0, place = 0
+    while (number > 0) {
+        result += (number % 10) * (Math.pow(8, place++))
+        number = Math.floor(number / 10)
+    }
+    return result
+}
+
